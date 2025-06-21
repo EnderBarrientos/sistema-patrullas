73278f16055b607fabd0678ed89dc5957eb04472
SusanDepablos <susandepablos14@gmail.com>
2025-06-21 16:44:25 -0400

    actualizacion

--- app.py
+++ app.py
@@ -654,9 +654,6 @@ def show_patrol(id_leader):
 @app.route('/delete-leader/<int:id_leader>', methods=['POST'])
 @login_required
 def delete_leader(id_leader):
-    if not current_user.is_admin:
-        flash('No tienes permisos para realizar esta acción', 'danger')
-        return redirect(url_for('dashboard'))
     
     leader = PatrolLeader.query.get_or_404(id_leader)
     
